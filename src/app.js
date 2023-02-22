48856375ef873181ef4b828c79409822defa8021
josephredmond5 <josephredmond5@icloud.com>
2023-02-22 21:23:12 +0000

    added a route to port 3000 in app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
 const express = require('express');
-
 const app = express();
-
+const port = 3000;
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello World');
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
 
 module.exports = app;
