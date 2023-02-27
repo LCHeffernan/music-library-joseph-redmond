bba15df084bcedadd85d56f91cdfd6bfce0e5172
josephredmond5 <josephredmond5@icloud.com>
2023-02-27 21:33:06 +0000

    progress made on the music-library module

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -1,7 +1,21 @@
-function createArtist(req, res) {
-  res.status(201).send('Artist created successfully');
+const db = require('../../src/db/index.js');
+
+async function createArtist(req, res) {
+  const { name } = req.body; // assuming the name of the new artist is passed in the request body
+  const query = `INSERT INTO Artist (name) VALUES ('${name}')`;
+  try {
+    const result = await db.query(query);
+    res.status(201).send(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
 }
+
 
 module.exports = {
   createArtist,
 };
+
+
+// DO NOT GO FORWARD UNLESS YOU HAVE TAKEN NOTES ON THIS 
